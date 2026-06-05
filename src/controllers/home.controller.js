08744c825674cb0faf2240b9cb0d97e5b6ba155c
LuisReyes970304 <luisrafaelreyescaro1@gmail.com>
2026-06-05 10:54:45 -0500

    feat: new reservation completed

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,6 +1,6 @@
-import ReservationCard from "@components/ReservationCard";//THis is not were the #reservationCOntainer works
-import { getReservations, createReservation } from "@services/reservation.service"; // This is important
-import { getSession } from "@/utils"; //checked is not here
+import ReservationCard from "@components/ReservationCard";
+import { getReservations, createReservation } from "@services/reservation.service"; 
+import { getSession } from "@/utils"; 
 
 export const homeController = async () => {
   const container = document.querySelector("#reservationsContainer");
@@ -93,75 +93,12 @@ export const homeController = async () => {
     </div>
   `;
 
-    const gestionForm = `
-    <div class="form-creation">
-
-      <div class="bg-white p-8 rounded-lg shadow w-96">
-
-        <h1 class="text-3xl font-bold mb-5">
-          Crear nueva reserva
-        </h1>
-
-        <form id="innerForm">
-
-          <input
-            type="text"
-            id="workspace"
-            placeholder="Indique la sala A o B"
-            class="border w-full p-2 rounded mb-3"
-            required
-          >
-
-          <input
-            type="date"
-            id="date"
-            class="border w-full p-2 rounded mb-4"
-            required
-          >
-
-          <label>Hora de inicio </label>
-          <input
-            type="time"
-            id="startHour"
-            class="border w-full p-2 rounded mb-3"
-            required
-          >
-
-          <label>Hora de finalizacion </label>
-          <input
-            type="time"
-            id="endHour"
-            class="border w-full p-2 rounded mb-3"
-            required
-          >
-
-          <label>Reason</label>
-          <textarea
-            type="text"
-            id="reason"
-            class="border w-full p-2 rounded mb-3 resize-none"
-            required
-          ></textarea>
-          <button
-            class="bg-blue-600 text-white w-full py-2 rounded"
-            type="submit"
-          >
-            Crear
-          </button>
-
-        </form>
-
-      </div>
-
-    </div>
-  `;
-
-    const btnNuevaReserva = document.querySelector("#btnNuevaReserva");
-    btnNuevaReserva.addEventListener("click", () => {
+    const btnNuevaReserva = await document.querySelector("#btnNuevaReserva");
+    btnNuevaReserva.addEventListener("click", (e) => {
       document.body.innerHTML += createForm;
       let innerForm = document.querySelector("#innerForm");
 
-      innerForm.addEventListener("submit", async (e) => {
+      innerForm.addEventListener("submit", (e) => {
         e.preventDefault();
           const workspace = document.querySelector("#workspace").value.trim();
           const date = document.querySelector("#date").value.trim();
@@ -169,14 +106,10 @@ export const homeController = async () => {
           const endHour = document.querySelector("#endHour").value.trim();
           const reason = document.querySelector("#reason").value.trim();
 
-          const newData = await createReservation(
-            {workspace: workspace, date: date, startHour: startHour, endHour: endHour, reason: reason, status: "pending", userId: parseInt(user.id)}
+          const newData = createReservation(
+            {workspace: "Sala " + workspace, date: date, startHour: startHour, endHour: endHour, reason: reason, status: "pending", userId: parseInt(user.id)}
           );
       })
     })
 
-    const btnGestionar = document.querySelector("#btnGestionar");
-    btnGestionar.addEventListener("click", () => {
-      alert("working")
-    })
 };
